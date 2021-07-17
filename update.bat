04006765f0a6cd6a92c2fc40607449e8f96c933b
Igerbam <igerbam@gmail.com>
2021-07-17 22:27:31 +0200

    initialcommit

--- update.bat
+++ update.bat
@@ -11,7 +11,37 @@ git pull --rebase
 git push
 color 6
 title Initialcommit - updating...
-ping localhost -n 10 > nul
+ping localhost -n 1 > nul
+echo 10%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 20%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 30%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 40%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 50%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 60%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 70%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 80%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 90%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
+echo 100%
+echo -ne '\b\b\b'
+ping localhost -n 1 > nul
 color 2
 title Initialcommit - updating done!
 echo ------------------------------------------------------------------------------------------
